8ff880eee57f40b17fab9a70ed67a4bff28ac16a
Calphonar <Richardzejak@hotmail.com>
2021-05-02 12:55:33 +0200

    Seeded some departments into database

---
+++ KodTestQueenslabApi/Data/DbSeed.cs
@@ -0,0 +1,31 @@
+﻿using KodTestQueenslabApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KodTestQueenslabApi.Data
+{
+    public class DbSeed
+    {
+        public static void Seeder(Context context)
+        {
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+            context.SaveChanges();
+
+
+            var departments = new Department[]
+            {
+                new Department { Name = "Provisions", Details = "All sorts of food." },
+                new Department { Name = "Hobby", Details = "Outdoor activity stuff, sports and camping." },
+                new Department { Name = "Electronics", Details = "Computer-related things, including phones and entertainement media." },
+                new Department { Name = "Office", Details = "Office accessories, also books and arts." },
+                new Department { Name = "Clothing", Details = "All four seasons clothing, for her, him and youngsters." }
+            };
+
+            context.Departments.AddRange(departments);
+            context.SaveChanges();
+        }
+    }
+}
